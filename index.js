06b4950ca253d7dbcd4013d8562e2c1b5c68242d
simransingh96 <work.simran96>
2023-06-30 11:23:56 -0400

    adding my resume

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ var typed = new Typed("#element", {
   strings: ["<i>Web</i> Developer."],
   typeSpeed: 50,
 });
+
+// Adding resume
+const resumebtn = document.querySelector(".resume");
+
+resumebtn.addEventListener("click", function () {
+  var link = document.createElement("a");
+  link.href = "/images/resume.pdf";
+  link.download = "simran_resume.pdf";
+  link.target = "_blank";
+  link.click();
+});
